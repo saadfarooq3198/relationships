d6e5370ab624d93413bb67c494b40d685a2778d7
SaadFarooq <saad96farooq@gmail.com>
2022-11-26 23:02:45 +0500

    one to one and one to one inverse completed

--- app/Http/Controllers/PhoneController.php
+++ app/Http/Controllers/PhoneController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Phone;
 use Illuminate\Http\Request;
 
 class PhoneController extends Controller
 {
-    //
+    public function index(){
+        $user = User::find(1)->phone;
+        $phone = Phone::find(1)->user;
+        dd($phone);
+        dd($user);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\PhoneController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts', [PostController::class, 'index']);
+Route::get('phone', [PhoneController::class, 'index']);
